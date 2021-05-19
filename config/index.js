6c45531bfaadc61ae4c9b43188d99bc48742c066
Luis Vazquez <luvazpa@gmail.com>
2021-05-18 20:33:45 -0500

    deployment settings added

--- config/index.js
+++ config/index.js
@@ -1,11 +1,14 @@
-require('dotenv').config()
+if (process.env.NODE_ENV === 'development') {
+  require('dotenv').config()
+}
 
 const config = {
-  dev: process.env.DEV === 'true',
+  dev: process.env.NODE_ENV === 'development',
   port: process.env.PORT || 3001,
   dbName: process.env.DB_NAME,
   dbUser: process.env.DB_USER,
-  dbPassword: process.env.DB_PASSWORD
+  dbPassword: process.env.DB_PASSWORD,
+  dbHost: process.env.DB_HOST
 }
 
 module.exports = config
